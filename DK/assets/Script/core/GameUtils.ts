cef4f5e0be44f5a0834ad4f1d386ae8498720f8e
yue19870813 <yue19870813@163.com>
2018-02-10 19:54:39 +0800

    判断两个矩形碰撞后是否会倒塌

--- DK/assets/Script/core/GameUtils.ts
+++ DK/assets/Script/core/GameUtils.ts
@@ -15,21 +15,23 @@ export class GameUtils {
         // 如果矩形1的中心点到矩形2的中心的x轴水平距离小于矩形1宽与矩形2宽的和的一半
         // 并且矩形1的中心点到矩形2的中心的y轴垂直距离小于矩形1高与矩形2高的和的一半
         // 则认为两个矩形存在相交，即碰撞，否则没有碰撞。
-        if (Math.abs(rect1.x - rect2.x) <= (rect1.width/2 + rect2.width/2)
-         && Math.abs(rect1.y - rect2.y) <= (rect1.height/2 + rect2.height/2)) {
+        if (Math.abs(rect1.x - rect2.x) < (rect1.width/2 + rect2.width/2)
+         && Math.abs(rect1.y - rect2.y) < (rect1.height/2 + rect2.height/2)) {
             return true;
         }
         return false;
     }
 
     /**
-     * 判断两个矩形碰撞后是否会倒塌
+     * 判断两个矩形碰撞后是否会倒塌,前提是已碰撞。
      * @param rect1 矩形1
      * @param rect2 矩形2
      * @returns {boolean} 是否会倒塌
      */
     public static isCrash (rect1:cc.Rect, rect2:cc.Rect):boolean {
-
+        if (Math.abs(rect1.x - rect2.x) > (rect1.width/2 + rect2.width/2)/2) {
+            return true;
+        }
         return false;
     }
 }
